054f80c9ae331b8f3fda8918ac745eed862d9d7a
Hudson Fernando <hudfernando@gmail.com>
2026-02-26 13:03:00 -0300

    modo hospitalar3

--- src/http/api.ts
+++ src/http/api.ts
@@ -2,5 +2,5 @@ import ky from 'ky'
 //baseURL: 'http://177.39.16.76:8405',
 export const api = ky.create({
   //prefixUrl: 'https://localhost:7286',
-  prefixUrl: 'http://goyazservice.ddns.me:18818'
+  prefixUrl: 'http://goyazservice.ddns.me:18819'
 })
